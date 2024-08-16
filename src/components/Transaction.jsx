1d62d187ce0f3ccf8b443e9274264e6c10060f82
Aditya Kumar <adityaakumar.in@gmail.com>
2024-08-17 02:52:25 +0530

    testing Click on Transaction

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -5,6 +5,7 @@ import { TransactionsContext } from './ElementProvider'
 const Transaction = ({ id, item, amount, isIncome }) => {
   const {transactions, setTransactions} = useContext(TransactionsContext);
   const [isHovered, setIsHovered] = useState(false);
+  const [isClicked, setIsClicked] = useState(false);
 
   const handleMouseOver = () => {
     setIsHovered(true);
@@ -43,8 +44,8 @@ const Transaction = ({ id, item, amount, isIncome }) => {
 
   return (
     <div className={`transaction ${isIncome ? "cash-in-border": "cash-out-border"}`} onMouseEnter={handleMouseOver} onMouseLeave={handleMouseOut}>
-      {isHovered ? <span>{date} - {time}</span> : <span>{item}</span>}
-      {isHovered ? <span onClick={()=>{removeCurrentId(id)}} style={{width: "27px", height: "27px"}}><svg xmlns="http://www.w3.org/2000/svg" height="27px" viewBox="0 -960 960 960" width="27px" fill="rgb(160,4,4)"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm80-160h80v-360h-80v360Zm160 0h80v-360h-80v360Z"/></svg></span> : <span>{isIncome ? "+ ₹" : "- ₹"}{`${amount}`}</span>}
+      {isHovered || isClicked ? <span>{date} - {time}</span> : <span>{item}</span>}
+      {isHovered || isClicked ? <span onClick={()=>{removeCurrentId(id); setIsClicked(!isClicked)}} style={{width: "27px", height: "27px"}}><svg xmlns="http://www.w3.org/2000/svg" height="27px" viewBox="0 -960 960 960" width="27px" fill="rgb(160,4,4)"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm80-160h80v-360h-80v360Zm160 0h80v-360h-80v360Z"/></svg></span> : <span>{isIncome ? "+ ₹" : "- ₹"}{`${amount}`}</span>}
     </div>
   )
 }
